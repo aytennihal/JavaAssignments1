bff2647a7599c20259ed940310110f2fe98597f3
aytennihal <80786330+aytennihal@users.noreply.github.com>
2021-03-20 04:29:19 -0500

    Add files via upload

---
+++ Queston5.java
@@ -0,0 +1,25 @@
+package assignments.day_2;
+
+public class Queston5 {
+
+	public static void main(String[] args) {
+	
+		
+		System.out.println("*******");
+		System.out.println("**   **");
+		System.out.println("* * * *");
+		System.out.println("*  *  *");
+		System.out.println("* * * *");
+		System.out.println("**   **");
+		System.out.println("*******");
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+}
